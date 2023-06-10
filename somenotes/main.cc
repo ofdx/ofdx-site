7dbaeed08f4adbba443ad344e0bc54d454f2775a
Mike Perron <mike@krakissi.net>
2023-06-10 00:40:06 +0000

    Use template files

--- somenotes/main.cc
+++ somenotes/main.cc
@@ -16,60 +16,17 @@
 
 class OfdxSomeNotes : public OfdxFcgiService {
 	void debugResponse(std::unique_ptr<dmitigr::fcgi::Server_connection> const& conn){
-		conn->out()
-			<< "Content-Type: text/html; charset=utf-8\r\n"
-			<< "\r\n"
-			<< "<!DOCTYPE html><html><head><title>OFDX - Debug</title></head><body>" << std::endl;
+		std::ifstream infile(m_cfg.m_templatePath + "debug.html");
 
-		if(!m_authUser.empty()){
-			// Greeting and logout link.
-			conn->out()
-				<< "<p>Welcome <b>" << m_authUser << "</b>!</p>" << std::endl
-				<< "<p><a href=\"" << URL_LOGOUT << "\">Logout</a>.</p>" << std::endl;
-		}
-
-		// Table of all parameters sent via FCGI
-		conn->out() << "<table><tr><th>Name</th><th>Value</th></tr>";
-		for(auto i = 0; i < conn->parameter_count(); ++ i){
-			conn->out() << "<tr><td>" << conn->parameter_name(i) << "</td><td>" << conn->parameter(i) << "</td></tr>";
-		}
-		conn->out() << "</table>" << std::endl;
-
-		// FIXME debug - Dump data path files
-		{
-			conn->out() << "<table><tr><th>Filename</th><th>Size</th><th>Path</th></tr>";
-
-			for(auto const& entry : std::filesystem::recursive_directory_iterator(m_cfg.m_dataPath)){
-				conn->out()
-					<< "<tr><td>" << entry.path().filename()
-					<< "</td><td>" << (entry.is_directory() ? std::string("<i>dir</i>") : std::to_string(entry.file_size()))
-					<< "</td><td>" << entry.path()
-					<< "</td></tr>";
-			}
-
-			conn->out() << "</table>" << std::endl;
-		}
-
-		conn->out() << "</body></html>";
+		if(infile)
+			serveTemplatedDocument(conn, infile);
 	}
 
 	void loginResponse(std::unique_ptr<dmitigr::fcgi::Server_connection> const& conn){
-		if(m_authUser.empty()){
-			// Show login page.
-			conn->out()
-				<< "Content-Type: text/html; charset=utf-8\r\n"
-				<< "\r\n"
-				<< "<!DOCTYPE html><html><head><title>OFDX - Login</title></head><body>" << std::endl
-				<< "<script src=\"/ofdx/js/ofdx_async.js\"></script>" << std::endl
-				<< "<script src=\"/ofdx/aaa/ofdx_auth.js\"></script>" << std::endl
+		std::ifstream infile(m_cfg.m_templatePath + "login.html");
 
-				<< "<form id=ofdx_login method=POST action=" << URL_LOGIN << ">"
-				<< "<label for=" << OFDX_USER << ">Username: </label><input id=" << OFDX_USER << " name=" << OFDX_USER << "><br>"
-				<< "<label for=" << OFDX_PASS << ">Password: </label><input id=" << OFDX_PASS << " name=" << OFDX_PASS << " type=password><br>"
-				<< "<input type=submit value=\"Login\"><br>"
-				<< "</form>" << std::endl
-
-				<< "</body></html>" << std::endl;
+		if(infile && m_authUser.empty()){
+			serveTemplatedDocument(conn, infile);
 		} else {
 			conn->out()
 				<< "Status: 500 Error\r\n"
@@ -81,10 +38,71 @@ class OfdxSomeNotes : public OfdxFcgiService {
 
 	void fillTemplate(std::unique_ptr<dmitigr::fcgi::Server_connection> const& conn, std::string const& text) override {
 		std::stringstream tss(text);
-		std::string word;
+		std::string area;
 
-		while(tss >> word)
-			conn->out() << "[" << word << "] ";
+		if(tss >> area){
+			if(area == "auth"){
+				// Related to authentication in some way.
+				if(tss >> area){
+					// Replace with the authenticated user's name
+					if(area == "user")
+						conn->out() << m_authUser;
+				}
+			} else if(area == "debug"){
+				// Debug stuff, probably going to be removed in the future.
+				if(tss >> area){
+					if(area == "table"){
+						// Display a debug informational table.
+						if(tss >> area){
+							if(area == "parameters"){
+								// Table of all parameters sent via FCGI
+								conn->out() << "<table><tr><th>Name</th><th>Value</th></tr>";
+								for(auto i = 0; i < conn->parameter_count(); ++ i){
+									conn->out() << "<tr><td>" << conn->parameter_name(i) << "</td><td>" << conn->parameter(i) << "</td></tr>";
+								}
+								conn->out() << "</table>";
+							} else if(area == "files"){
+								// Dump out all of the files in the working directory.
+								conn->out() << "<table><tr><th>Filename</th><th>Size</th><th>Path</th></tr>";
+
+								for(auto const& entry : std::filesystem::recursive_directory_iterator(m_cfg.m_dataPath)){
+									conn->out()
+										<< "<tr><td>" << entry.path().filename()
+										<< "</td><td>" << (entry.is_directory() ? std::string("<i>dir</i>") : std::to_string(entry.file_size()))
+										<< "</td><td>" << entry.path()
+										<< "</td></tr>";
+								}
+
+								conn->out() << "</table>";
+							}
+						}
+					} else if(area == "isauthd"){
+						// Serve the next templated file if the user is authenticated.
+						if(!m_authUser.empty()){
+							std::string fname;
+
+							if(tss >> fname){
+								std::ifstream infile(m_cfg.m_templatePath + fname);
+
+								if(infile)
+									serveTemplatedDocument(conn, infile);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	void serve404(std::unique_ptr<dmitigr::fcgi::Server_connection> const& conn){
+		std::ifstream infile(m_cfg.m_templatePath + "404.html");
+
+		if(infile){
+			serveTemplatedDocument(conn, infile);
+		} else {
+			// Misconfiguration or missing file...
+			conn->out() << "Status: 404\r\n\r\n";
+		}
 	}
 
 public:
@@ -122,9 +140,7 @@ public:
 
 				serveTemplatedDocument(conn, infile);
 			} else {
-				conn->out()
-					<< "Status: 404 Not Found\r\n"
-					<< "\r\n";
+				serve404(conn);
 			}
 		} else if(SCRIPT_NAME == URL_NOTES_DEBUG){
 			if(m_authUser.empty()){
@@ -135,12 +151,7 @@ public:
 				debugResponse(conn);
 			}
 		} else {
-			// 404 for all other URIs
-			conn->out()
-				<< "Status: 404 Not Found\r\n"
-				<< "Content-Type: text/plain; charset=utf-8\r\n"
-				<< "\r\n"
-				<< "No notes here.";
+			serve404(conn);
 		}
 	}
 };
