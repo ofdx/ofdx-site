8ff0fb81849ab6a984c06b46ad2ab0a0fd314ae6
Mike Perron <mike@krakissi.net>
2023-11-03 03:50:28 +0000

    NoteFile Database Beginnings

--- somenotes/main.cc
+++ somenotes/main.cc
@@ -13,8 +13,118 @@
 
 #include <filesystem>
 
-
 class OfdxSomeNotes : public OfdxFcgiService {
+	class NoteDatabase {
+		std::string m_dbpath;
+
+		struct NoteFile {
+			// Every NoteFile is represented by a file on disk, named m_id.
+			// The file should have headers for each of the members of this
+			// struct, plus the actual contents of the file (as m_body).
+			uint64_t m_timeCreated, m_timeModified;
+			std::string m_id, m_author, m_title, m_body;
+
+			// When a file is modified, we can mark it as pendingSave, and
+			// eventually it will be written to disk.
+			bool m_pendingSave;
+
+			NoteFile() :
+				m_pendingSave(false)
+			{}
+		};
+
+		std::unordered_map<std::string, std::shared_ptr<NoteFile>> m_notes;
+
+		void get_the_rest(std::stringstream &src, std::string &dest) const {
+			if(src >> dest){
+				std::string buf;
+
+				if(getline(src, buf))
+					dest += buf;
+			}
+		}
+
+	public:
+		void load(){
+			// Open m_dbpath and read every file.
+			for(auto const& entry : std::filesystem::recursive_directory_iterator(m_dbpath)){
+				if(entry.is_regular_file()){
+					std::ifstream infile(entry.path());
+
+					if(infile){
+						std::shared_ptr<NoteFile> note = std::make_shared<NoteFile>();
+
+						// The ID is simply the file name.
+						note->m_id = entry.path().filename();
+
+						// Read the headers
+						{
+							std::string line;
+							while(getline(infile, line)){
+								// End of headers.
+								if(line.empty())
+									break;
+
+								std::stringstream hss(line);
+								std::string param;
+
+								if(hss >> param){
+									if(param == "author")
+										get_the_rest(hss, note->m_author);
+									else if(param == "title")
+										get_the_rest(hss, note->m_title);
+									else if(param == "created")
+										hss >> note->m_timeCreated;
+									else if(param == "modified")
+										hss >> note->m_timeModified;
+								}
+							}
+
+							// Data repair as needed...
+							if(note->m_timeModified < note->m_timeCreated)
+								note->m_timeModified = note->m_timeCreated;
+						}
+
+						// Read body and store it.
+						if(infile){
+							std::stringstream body;
+							std::string line;
+
+							while(getline(infile, line))
+								body << line << '\n';
+
+							note->m_body = body.str();
+						}
+
+						// Store that bad boy in the map.
+						m_notes[note->m_id] = note;
+					}
+				}
+			}
+		}
+
+		void save(){
+			// TODO - write out entire database to disk. Or write one file?
+			// The persistence should probably be granual.
+			// Modification time management?
+		}
+
+		// FIXME debug
+		void debug(std::ostream &ss){
+			for(auto &el : m_notes){
+				ss << "[" << el.first << "] id[" << el.second->m_id << "] author[" << el.second->m_author << "] "
+				<< "title[" << el.second->m_title << "] created[" << el.second->m_timeCreated << "] modified[" << el.second->m_timeModified << "] "
+				<< "body[EOF]\n" << el.second->m_body << "EOF\n";
+			}
+		}
+
+		NoteDatabase(std::string const& dbpath) :
+			m_dbpath(dbpath)
+		{
+			load();
+		}
+	} *m_noteDb;
+
 	void debugResponse(std::unique_ptr<dmitigr::fcgi::Server_connection> const& conn){
 		if(!serveTemplatedDocument(conn, (m_cfg.m_templatePath + "debug/infopage.html"), true))
 			serve404(conn);
@@ -58,18 +168,8 @@ class OfdxSomeNotes : public OfdxFcgiService {
 						}
 						conn->out() << "</table>";
 					} else if(word == "files"){
-						// Dump out all of the files in the working directory.
-						conn->out() << "<table><tr><th>Filename</th><th>Size</th><th>Path</th></tr>";
-
-						for(auto const& entry : std::filesystem::recursive_directory_iterator(m_cfg.m_dataPath)){
-							conn->out()
-								<< "<tr><td>" << entry.path().filename()
-								<< "</td><td>" << (entry.is_directory() ? std::string("<i>dir</i>") : std::to_string(entry.file_size()))
-								<< "</td><td>" << entry.path()
-								<< "</td></tr>";
-						}
-
-						conn->out() << "</table>";
+						// Dump the file database.
+						m_noteDb->debug(conn->out());
 					}
 				} else if(word == "isauthd"){
 					// Serve the next templated file if the user is authenticated.
@@ -107,9 +207,9 @@ public:
 	} m_cfg;
 
 	OfdxSomeNotes() :
+		m_noteDb(nullptr),
 		m_cfg(PORT_OFDX_SOMENOTES, PATH_OFDX_SOMENOTES)
-	{
-	}
+	{}
 
 	bool processCliArguments(int argc, char **argv){
 		if(m_cfg.processCliArguments(argc, argv)){
@@ -119,6 +219,8 @@ public:
 				return false;
 			}
 		}
+
+		m_noteDb = new NoteDatabase(m_cfg.m_dataPath);
 
 		return true;
 	}
