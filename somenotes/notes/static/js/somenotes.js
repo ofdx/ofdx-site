f6dd16f7852fc6ca31f3d7eabf97f25c9b7b5432
Mike Perron <mike@krakissi.net>
2024-05-05 18:39:15 +0000

    Load last note from the server

--- somenotes/notes/static/js/somenotes.js
+++ somenotes/notes/static/js/somenotes.js
@@ -3,15 +3,63 @@
 	mperron (2024)
 */
 
+var OfdxSomeNotes = {
+	note: {}
+};
+
 window.addEventListener('load', function(){
 	// Main textarea
 	{
 		var note_texted_field = document.getElementById('note_texted_field');
+		var note_texted_title = document.getElementById('note_texted_doc_title');
 
-		// Save/restore state of text editor.
+		// Preload the field with the last value from localStorage, but disable
+		// it until we get the latest from the server.
 		note_texted_field.value = localStorage.getItem('note_texted_field_value');
+		note_texted_field.disabled = true;
+
+		// Get the latest version of the last note from the server
+		let notename = localStorage.getItem('note_texted_lastfile');
+		if(notename){
+			OfdxAsync.send({
+				target: 'f/' + notename,
+				type: 'json',
+				completion: function(http){
+					// FIXME debug
+					console.log(http);
+
+					if(http.status === 200){
+						// Store this response for future reference.
+						OfdxSomeNotes.note = http.response;
+
+						// Update fields on the page.
+						note_texted_field.value = atob(http.response.body);
+						note_texted_title.value = atob(http.response.title);
+					} else {
+						// FIXME debug
+						alert('note not loaded...');
+
+						// TODO handle if file does not exist on the server
+					}
+
+					note_texted_field.disabled = false;
+				},
+				onerror: function(http){
+					// FIXME debug
+					console.log('error!', http);
+				}
+			});
+		} else {
+			note_texted_field.disabled = false;
+		}
+
+		// Store changes.
 		note_texted_field.addEventListener('change', function(e){
+			// Store locally immediately.
 			localStorage.setItem('note_texted_field_value', note_texted_field.value);
+
+			// Store remotely after a timeout to minimize server impact.
+			// TODO
 		});
 	}
 
